3bdaecf7356bb52c422299df01adfa5e3d3cd8fd
Darrion <52856180+Darrionat@users.noreply.github.com>
2021-05-17 15:26:50 -0400

    1.1.2
    Bug Fixes:
     - BaseCommand will now be able to handle situations where no arguments are given

--- src/main/java/me/darrionat/pluginlib/commands/BaseCommand.java
+++ src/main/java/me/darrionat/pluginlib/commands/BaseCommand.java
@@ -16,77 +16,78 @@ import me.darrionat.pluginlib.Plugin;
  * The {@link BaseCommand} class represents a registered {@link PluginCommand}.
  */
 public abstract class BaseCommand implements CommandExecutor, HeritableCommand {
-	private ErrorHandler errorHandler;
-	private String permission;
-	private List<SubCommand> subCommands = new ArrayList<SubCommand>();
+    private ErrorHandler errorHandler;
+    private String permission;
+    private List<SubCommand> subCommands = new ArrayList<SubCommand>();
 
-	/**
-	 * Creates and registers a new {@link BaseCommand} object.
-	 * 
-	 * @param plugin The {@link Plugin} that the command will be registered to.
-	 */
-	public BaseCommand(Plugin plugin) {
-		this.errorHandler = plugin.getErrorHandler();
-		this.permission = getCommandLabel() + ".use";
-		plugin.getCommand(getCommandLabel()).setExecutor(this);
-	}
+    /**
+     * Creates and registers a new {@link BaseCommand} object.
+     * 
+     * @param plugin The {@link Plugin} that the command will be registered to.
+     */
+    public BaseCommand(Plugin plugin) {
+        this.errorHandler = plugin.getErrorHandler();
+        this.permission = getCommandLabel() + ".use";
+        plugin.getCommand(getCommandLabel()).setExecutor(this);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		// Permission
-		if (sender instanceof Player) {
-			Player p = (Player) sender;
-			if (!p.hasPermission(permission)) {
-				errorHandler.noPermissionError(p, permission);
-				return true;
-			}
-		}
+    /**
+     * {@inheritDoc}
+     */
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Permission
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (!p.hasPermission(permission)) {
+                errorHandler.noPermissionError(p, permission);
+                return true;
+            }
+        }
 
-		// Subcommands
-		if (args.length == 0) {
+        // Subcommands
+        if (args.length == 0) {
+            runNoArgs(sender, command, label, args);
+            return true;
+        }
 
-			runNoArgs(sender, command, label, args);
-		}
+        for (SubCommand subCommand : getSubCommands())
+            if (args[0].equalsIgnoreCase(subCommand.getSubCommand())) {
+                subCommand.run(sender, args);
+                return true;
+            }
+        // No suitable subcommand
+        runNoArgs(sender, command, label, args);
+        return true;
+    }
 
-		for (SubCommand subCommand : getSubCommands())
-			if (args[0].equalsIgnoreCase(subCommand.getSubCommand())) {
-				subCommand.run(sender, args);
-				return true;
-			}
-		return true;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public List<SubCommand> getSubCommands() {
+        return subCommands;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public List<SubCommand> getSubCommands() {
-		return subCommands;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public void addSubCommand(SubCommand subCommand) {
+        subCommands.add(subCommand);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void addSubCommand(SubCommand subCommand) {
-		subCommands.add(subCommand);
-	}
+    /**
+     * Gets the label of the {@link BaseCommand}.
+     * 
+     * @return Returns the label of the command as a {@link String}.
+     */
+    public abstract String getCommandLabel();
 
-	/**
-	 * Gets the label of the {@link BaseCommand}.
-	 * 
-	 * @return Returns the label of the command as a {@link String}.
-	 */
-	public abstract String getCommandLabel();
-
-	/**
-	 * Ran when the {@link BaseCommand} has no {@link SubCommand}s that were run.
-	 * 
-	 * @param sender  the sender of the command
-	 * @param command the command that was ran
-	 * @param label   the label of the command
-	 * @param args    the arguments that were used
-	 */
-	protected abstract void runNoArgs(CommandSender sender, Command command, String label, String[] args);
-
+    /**
+     * Ran when the {@link BaseCommand} has no {@link SubCommand}s that were run.
+     * 
+     * @param sender  the sender of the command
+     * @param command the command that was ran
+     * @param label   the label of the command
+     * @param args    the arguments that were used
+     */
+    protected abstract void runNoArgs(CommandSender sender, Command command, String label, String[] args);
 }
